2bbd72e1f00e6ca0e638214833923b5bba35a220
navandstokes <sherrii.cui@pm.me>
2023-05-04 16:16:20 +0200

    linter

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,44 @@ export default function Home() {
   return (
     <div>
       <p>Static</p>
-      <Image src={`/api`} width="200" height="200" />
+      <Image src={`/api`} width="200" height="200" alt="an image" />
       <p>Dynamic Segment</p>
-      <Image src={`/api/dynamic-segment/jfkdlsjfds`} width="200" height="200" />
+      <Image
+        src={`/api/dynamic-segment/jfkdlsjfds`}
+        width="200"
+        height="200"
+        alt="an image"
+      />
       <p>Header</p>
-      <Image src={`/api/header/page`} width="200" height="200" />
+      <Image src={`/api/header/page`} width="200" height="200" alt="an image" />
       <p>Dynamic Params</p>
-      <Image src={`/api/dynamic-params/page`} width="200" height="200" />
+      <Image
+        src={`/api/dynamic-params/page`}
+        width="200"
+        height="200"
+        alt="an image"
+      />
       <p>Force Dynamic</p>
-      <Image src={`/api/force-dynamic/page`} width="200" height="200" />
+      <Image
+        src={`/api/force-dynamic/page`}
+        width="200"
+        height="200"
+        alt="an image"
+      />
       <p>Revalidate 0</p>
-      <Image src={`/api/revalidate/page`} width="200" height="200" />
+      <Image
+        src={`/api/revalidate/page`}
+        width="200"
+        height="200"
+        alt="an image"
+      />
       <p>Edge Runtime</p>
-      <Image src={`/api/edge-runtime/page`} width="200" height="200" />
+      <Image
+        src={`/api/edge-runtime/page`}
+        width="200"
+        height="200"
+        alt="an image"
+      />
     </div>
   );
 }
